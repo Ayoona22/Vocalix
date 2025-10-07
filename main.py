a20e56b305894f9fcc369f939d68d4ea1266cda3
Dona Babu <159267943+Donab01@users.noreply.github.com>
2025-10-07 13:51:58 +0530

    I have  uploaded the frontend and backend code files for the real time chatting interface

---
+++ main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from pydantic import BaseModel
+
+import requests
+
+PRIVATE_KEY = "8c63dbce-80a7-455a-890b-9368d16e1dcb" 
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_orgins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+PRIVATE_KEY = "....."
+
+class User(BaseModel):
+    username: str
+
+@app.post('/authenticate')
+async def authenticate(user: User):
+    resopnse = requests.put('https://api.chatengine.io/users/',
+                            data = {
+                                "username": user.username,
+                                "secret": user.username,
+                                "first_name": user.username,
+                            },
+                            headers={ "Private-Key": PRIVATE_KEY }
+                        )
+    return response.json()
+
